9ee14a0fb67ca8424a73721b35d8e2598eb28f3f
JEONG-BD <dev.byeongdu@gmail.com>
2024-03-17 12:40:09 +0900

    2024-03-17 variable

--- src/Ch01/VarEx01.java
+++ src/Ch01/VarEx01.java
@@ -1,4 +1,21 @@
 package Ch01;
 
 public class VarEx01 {
+    public static void main(String[] args){
+
+    /*
+        variable 변수
+    */
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 2000 ;
+        age = age + 1;
+
+        System.out.println(year);
+        System.out.println(age);
+    }
 }
